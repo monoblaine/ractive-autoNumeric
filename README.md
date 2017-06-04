660072a84891e66e96d06a217635adae88d9caea
Serhan Apaydın <zerhan@gmail.com>
2017-06-05 01:05:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# About
+
+This is a [Ractive.js](https://ractive.js.org/) decorator for [autoNumeric](https://github.com/autoNumeric/autoNumeric).
+
+# Demo
+
+http://jsfiddle.net/monoblaine/6kb433Lt/20/
+
+# Usage
+
+1. Include source files for ractive.js, jquery.js, autoNumeric.js, ractive-autoNumeric.js, etc.
+2. Create your ractive template and the ractive instance:
+
+## Template
+
+```html
+<div id="el"></div>
+
+<script type="text/template" id="template">
+    <input type="text" as-autoNumeric="'foo', foo">
+</script>
+```
+
+## Ractive instance
+
+```js
+new Ractive({
+    el: 'el',
+    template: '#template',
+
+    data: {
+        foo: 123.45
+    }
+});
+```
+
+# TODO
+
+* Needs to be modularized, ES6-compatible, etc.
---
+++ src/ractive-autoNumeric.js
@@ -0,0 +1,41 @@
+Ractive.decorators.autoNumeric = function (node, relativeKeypath, boundValue) {
+    var self = this,
+        $el = $(node),
+        context = this.getContext(node),
+        updateModel = function () {
+            context.set(relativeKeypath, $el.autoNumeric('getNumber'));
+        };
+
+    if (typeof relativeKeypath !== 'string') {
+        throw new Error('The binding name is required.');
+    }
+
+    if (arguments.length !== 3) {
+        throw new Error('Invalid number of arguments specified. Correct usage: as-autoNumeric="\'relativeKeypath\', relativeKeypath"');
+    }
+
+    if (Ractive.getContext(node).isBound()) {
+        throw new Error('You cannot use two-way binding for the "autoNumeric" decorator.');
+    }
+
+    $el.autoNumeric('init')
+       .autoNumeric('set', boundValue)
+       .on('input', updateModel);
+
+    return {
+        update: function (newRelativeKeypath, newValue) {
+            if (newRelativeKeypath !== relativeKeypath) {
+                relativeKeypath = newRelativeKeypath;
+            }
+
+            if ($el.autoNumeric('getNumber') !== newValue) {
+                $el.autoNumeric('set', newValue);
+            }
+        },
+
+        teardown: function () {
+            $el.off('input', updateModel)
+               .autoNumeric('destroy');
+        }
+    };
+};
